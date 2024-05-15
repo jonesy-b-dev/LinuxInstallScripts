a8f8db4914151f96d997c4f3dbbbe1f593d6fd2c
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-05-15 14:39:03 +0200

    Add Arch install script

---
+++ installArch.sh
@@ -0,0 +1,28 @@
+# Install yay AUR
+sudo pacman -S git
+git clone https://aur.archlinux.org/yay-git.git && cd yay
+makepkg -si
+
+#install Brave
+yay -S brave-bin
+
+# Install other packages
+sudo pacman -S vim neovim unzip alacritty feh rofi lxappearance ttf-font-awesome polybar tree picom
+
+# Install font
+cd ~
+mkdir downloads
+cd downloads
+curl -L https://github.com/ryanoasis/nerd-fonts/releases/download/v3.2.1/Hermit.zip
+mkdir /usr/share/fonts/Hermit
+unzip Hermit.zip -d /usr/share/fonts/Hermit
+
+# Setup configs
+cd ~
+mkdir dev
+cd dev
+mkdir other
+cd other
+git clone https://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@github.com/jonesy-b-dev/LinuxConfigFiles
+cd LinuxConfigFiles
+lcf.sh -ibn
