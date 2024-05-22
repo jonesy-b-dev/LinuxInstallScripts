b4c8f9c71740aa2c17aa49304d312f56dbf28470
Jonas de Bruin <59418915+jonesy-b-dev@users.noreply.github.com>
2024-05-22 12:54:34 +0200

    Added ripgrep and vi to arch install

--- installArch.sh
+++ installArch.sh
@@ -7,7 +7,7 @@ makepkg -si
 yay -S brave-bin
 
 # Install other packages
-sudo pacman -S vim neovim unzip alacritty feh rofi lxappearance ttf-font-awesome polybar tree picom cmake
+sudo pacman -S vi vim ripgrep neovim unzip alacritty feh rofi lxappearance ttf-font-awesome polybar tree picom cmake
 
 # Install font
 cd ~
